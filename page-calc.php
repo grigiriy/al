31f292a90aad6a727706055da5152b72d959dbd8
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-07 10:37:07 +0300

    default structure

---
+++ page-calc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Calc
+ */
+get_header(); 
+
+while ( have_posts() ) :
+    the_post();
+
+
+echo '<h1>Headline 1</h1>';
+get_template_part('theme-helpers/template-parts/firstScreen');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/calc');
+get_template_part('theme-helpers/template-parts/textBlock');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/terms');
+get_template_part('theme-helpers/template-parts/textBlock');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/order');
+endwhile;
+get_footer();
+?>
