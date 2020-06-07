06ccf1667f308d3d470a16ca41227bc7584f8837
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-07 16:17:06 +0300

    first screen

--- theme-helpers/template-parts/firstScreen.php
+++ theme-helpers/template-parts/firstScreen.php
@@ -1,54 +1,48 @@
-<section class="background-f2 uk-overflow-hidden main-block">        
+<section class="main-block pt-5">        
 
-  <div class="uk-container uk-position-relative">
+  <div class="container-fluid">
 
-    <div class="scene-parallax" id="scene1" style="transform: translate3d(0px, 0px, 0px) rotate(0.0001deg); transform-style: preserve-3d; backface-visibility: hidden; position: relative; pointer-events: none;">        
+    <!-- <div class="scene-parallax" id="scene1" style="transform: translate3d(0px, 0px, 0px) rotate(0.0001deg); transform-style: preserve-3d; backface-visibility: hidden; position: relative; pointer-events: none;">        
       <div class="layer" data-depth="0.4" style="transform: translate3d(20px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: relative; display: block; left: 0px; top: 0px;"><div class="back-circ1 circleanim"></div></div>
       <div class="layer" data-depth="-0.2" style="transform: translate3d(-10px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><div class="back-circ2 circleoanim"></div></div>
       <div class="layer" data-depth="0.2" style="transform: translate3d(10px, 0px, 0px); transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block; left: 0px; top: 0px;"><div class="back-circ3 circletanim"></div></div>
-    </div>
+    </div> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-12">
+          <h1><?= get_headline($post->ID,$post->post_parent,'0') ?></h1>
 
-    <div class="uk-grid uk-position-relative" uk-grid="">
-      <div class="uk-width-1-1 uk-width-4-5@m uk-width-3-5@l padding-main uk-first-column">
-
-        <h1><?= get_headline($post->ID,$post->post_parent,'0') ?></h1>
-        <p>Автоломбард в городе Москва выдает деньги под залог ПТС автомобиля за&nbsp;30&nbsp;минут&nbsp;на&nbsp;выгодных для клиента условиях.</p>
-
-        <div class="uk-child-width-auto uk-grid-default uk-margin uk-flex font-weight-700 uk-grid" uk-grid="">
-
-          <div class="icon-horizon uk-first-column">
-            <div class="icon-horizon-text">                                      
-              <div class="uk-margin-remove-bottom">Займы до 3 000 000 ₽</div>       
-              <div class="deepblue">Авто остаётся у вас</div> 
+          <div class="row ml-lg-n5">
+            <div class="col-lg-6 col-12 d-flex first_screen_list mb-3">
+              <img src="<?= get_template_directory_uri(); ?>/css/images/check.svg"" alt="">
+              <p>До 5 000 000 рублей на срок до 3 лет</p>
             </div>
-          </div>     
-
-
-          <div class="icon-horizon">
-            <div class="icon-horizon-text">                                      
-              <div class="uk-margin-remove-bottom">100% одобрения</div>       
-              <div class="deepblue">Ставка от 2% в месяц</div> 
+            <div class="col-lg-6 col-12 d-flex first_screen_list mb-3">
+              <img src="<?= get_template_directory_uri(); ?>/css/images/check.svg"" alt="">
+              <p>С любой кредитной историей</p>
             </div>
-          </div>     
-
+            <div class="col-lg-6 col-12 d-flex first_screen_list mb-3">
+              <img src="<?= get_template_directory_uri(); ?>/css/images/check.svg"" alt="">
+              <p>Без справок с места работы и поручителей</p>
+            </div>
+            <div class="col-lg-6 col-12 d-flex first_screen_list mb-3">
+              <img src="<?= get_template_directory_uri(); ?>/css/images/check.svg"" alt="">
+              <p>После выбычи займа авто остается у вас</p>
+            </div>
+            <div class="col-lg-6 col-12 d-flex first_screen_list mb-3">
+              <img src="<?= get_template_directory_uri(); ?>/css/images/check.svg"" alt="">
+              <p>Одобрение за 5 минут, выдача за 30 минут</p>
+            </div>
+            <div class="col-lg-6 col-12 d-flex my-1">
+              <a class="uk-button uk-button-primary uk-margin-right uk-margin-bottom" href="#modal-center1" uk-toggle="">Оставить заявку</a>
+            </div>
+          </div>   
         </div>
-
-        <a class="uk-button uk-button-primary uk-margin-right uk-margin-bottom" href="#modal-center1" uk-toggle="">Оставить заявку</a>
       </div>
-
-      <div class="uk-width-1-1 uk-width-1-5@m uk-width-2-5@l">                 
-        <img class="bluecar uk-scrollspy-inview uk-animation-slide-right" src="<?= get_template_directory_uri(); ?>/css/images/car-blue.png" uk-scrollspy="cls: uk-animation-slide-right; delay: 1000" style="" width="1000" height="512">
-      </div>
+      <img class="ml-5 mt-5 bluecar uk-scrollspy-inview uk-animation-slide-right" src="<?= get_template_directory_uri(); ?>/css/images/car-blue.png" uk-scrollspy="cls: uk-animation-slide-right; delay: 1000" style="" width="1000" height="512">
     </div>
   </div>
 </section>
-
-<pre>
-<?php
-print_r($post)
-?>
-
-</pre>
 
 <?php get_template_part('theme-helpers/template-parts/form','horizontal'); ?>
 
