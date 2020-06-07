afe72c52e6af186c10228505ddcd9cba45c82b6a
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-07 11:13:28 +0300

    lil correction for structure

---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Main
+ */
+get_header(); 
+
+while ( have_posts() ) :
+    the_post();
+
+
+echo '<h1>Headline 1</h1>';
+get_template_part('theme-helpers/template-parts/firstScreen');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/types');
+get_template_part('theme-helpers/template-parts/textBlock');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/advantages');
+get_template_part('theme-helpers/template-parts/textBlock');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/calc');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/terms');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/requirements');
+get_template_part('theme-helpers/template-parts/textBlock');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/order');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/terms');
+
+echo '<h2>Headline 2</h2>';
+get_template_part('theme-helpers/template-parts/faq');
+endwhile;
+get_footer();
+?>
