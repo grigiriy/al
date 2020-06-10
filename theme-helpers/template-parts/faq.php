82fe630262ef0ca2baf7170cd813ab44e54e5d8e
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-10 09:54:05 +0300

    accordion

--- theme-helpers/template-parts/faq.php
+++ theme-helpers/template-parts/faq.php
@@ -4,12 +4,14 @@
     <h2 class="uk-text-center uk-h2">Ответы на вопросы</h2>
     <p class="uk-text-center weight300 padding-lr-50">Вопросы о деятельности автоломбарда в Москве. Мы собрали самые популярные вопросы и подробно ответили на них.</p>
 
-    <ul uk-accordion="" class="uk-accordion">
+    <ul id="faq_accordion" class="uk-accordion">
     <?php $faqs = carbon_get_theme_option( $faq_type.'_faq' );
-    foreach ($faqs as $faq){ ?>
+    foreach ($faqs as $key=>$faq){ ?>
       <li class="uk-open">
-        <a class="uk-accordion-title" href="#"><h3><?= apply_filters( 'the_content', $faq['subtitle'] ); ?></h3></a>
-        <div class="uk-accordion-content" aria-hidden="false">
+        <a id="heading_<?= $key; ?>" class="uk-accordion-title" data-toggle="collapse" data-target="#collapse_<?= $key; ?>" aria-expanded="false" aria-controls="collapse_<?= $key; ?>">
+          <h3><?= apply_filters( 'the_content', $faq['subtitle'] ); ?></h3>
+        </a>
+        <div id="collapse_<?= $key; ?>" class="uk-accordion-content collapse <?= ($key === 0) ? 'show' : ''; ?>" aria-labelledby="heading_<?= $key; ?>" data-parent="#faq_accordion">
           <p><?= apply_filters( 'the_content', $faq['text']); ?></p>
         </div>
       </li>
