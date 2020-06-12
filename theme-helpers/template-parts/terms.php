ac547843855aae1991e3d3a5159fab6a63dc8b8c
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-12 17:01:44 +0300

    links decor and change bullets on terms

--- theme-helpers/template-parts/terms.php
+++ theme-helpers/template-parts/terms.php
@@ -10,20 +10,20 @@
 
         <div class="row">
             <div class="col-xl-4 col-md-4 offset-sm-0 offset-md-2 col-sm-12">
-                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/arrow.png" alt="arrow">Сумма займа:</p>
+                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/tick.png" alt="arrow">Сумма займа:</p>
                 <p class="conditions-two-text"> от 25 000 до <br class="br-mobile">10 000 000 рублей</p>
             </div>
             <div class="col-xl-4 col-md-4 offset-sm-0 offset-md-1 col-sm-12">
-                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/arrow.png" alt="arrow">Срок займа: </p>
+                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/tick.png" alt="arrow">Срок займа: </p>
                 <p class="conditions-two-text">от 1 до 36 месяцев</p>
             </div>
             <div class="col-xl-4 col-md-4 offset-sm-0 offset-md-2 col-sm-12">
-                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/arrow.png" alt="arrow">Процентная ставка: </p>
+                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/tick.png" alt="arrow">Процентная ставка: </p>
                 <p class="conditions-two-text"> от 2% до 3,5% в месяц</p>
                 <p class="conditions-3-text">(от 24 до 42% годовых)</p>
             </div>
             <div class="col-xl-4 col-md-4 offset-sm-0 offset-md-1 col-sm-12">
-                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/arrow.png" alt="arrow">Максимальная сумма займа:
+                <p class="conditions-one-text"> <img src="<?= get_template_directory_uri(); ?>/css/images/tick.png" alt="arrow">Максимальная сумма займа:
                 </p>
                 <p class="conditions-two-text">до 90% от рыночной стоимости автомобиля</p>
             </div>
