36a632d52abb9e00083e895587dfa2eeaec0dbb6
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-12 15:25:38 +0300

    new pics on types

--- theme-helpers/template-parts/types.php
+++ theme-helpers/template-parts/types.php
@@ -11,7 +11,7 @@
 
     <div class="uk-first-column">
 
-      <a class="small-card main-bg_onhov background-f2 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 5;" uk-toggle="">
+      <a class="small-card main-bg_onhov background-f2 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 1;" uk-toggle="">
         <div class="number-card">01</div>
         <div class="text-card">Легковой<br>автомобиль</div>
         <div class="card-img card-img1"><img src="<?= get_template_directory_uri(); ?>/css/images/card1.png" width="195" height="130"></div>
@@ -20,7 +20,7 @@
     </div>
     <div>
 
-      <a class="small-card main-bg_onhov background-f8 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 4;" uk-toggle="">
+      <a class="small-card main-bg_onhov background-f8 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 2;" uk-toggle="">
         <div class="number-card">02</div>
         <div class="text-card">Коммерческий<br>транспорт</div>
         <div class="card-img card-img2"><img src="<?= get_template_directory_uri(); ?>/css/images/card2.png" width="195" height="130"></div>
@@ -38,7 +38,7 @@
     </div>
     <div>
 
-      <a class="small-card main-bg_onhov background-f8 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 2;" uk-toggle="">
+      <a class="small-card main-bg_onhov background-f8 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 4;" uk-toggle="">
         <div class="number-card">04</div>
         <div class="text-card">СпецТехника</div>
         <div class="card-img card-img4"><img src="<?= get_template_directory_uri(); ?>/css/images/card4.png" width="195" height="130"></div>
@@ -48,7 +48,7 @@
 
     <div>
 
-      <a class="small-card main-bg_onhov background-f2 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 1;" uk-toggle="">
+      <a class="small-card main-bg_onhov background-f2 uk-flex uk-flex-between uk-flex-middle" href="#modal-center1" style="z-index: 5;" uk-toggle="">
         <div class="number-card">05</div>
         <div class="text-card">Грузовой<br>автомобиль</div>
         <div class="card-img card-img5"><img src="<?= get_template_directory_uri(); ?>/css/images/card5.png" width="195" height="130"></div>
