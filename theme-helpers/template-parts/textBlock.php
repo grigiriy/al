f37acb4ae635fb2301bfe49e54f0d741de4cea55
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-20 15:16:00 +0300

    ul centering on text-sections

--- theme-helpers/template-parts/textBlock.php
+++ theme-helpers/template-parts/textBlock.php
@@ -1,4 +1,4 @@
-<section <?= ( $text === 'advantages_text') ? 'class="bg-f2"' : '';?> >
+<section class="<?= ( $text === 'advantages_text') ? 'bg-f2 text-section' : 'text-section';?>" >
     <div class="container text-lightcolor">
         <?= apply_filters( 'the_content', wpautop(carbon_get_theme_option( $text_page.'_texts' )[0][$text] ) ); ?>
     </div>
