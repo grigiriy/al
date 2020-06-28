fdf500eda334befe215104f8696b5ab294975500
Grigiriy <grigiriy.malyshev@gmail.com>
2020-06-28 22:49:45 +0300

    form fixes

--- theme-helpers/template-parts/form-horizontal.php
+++ theme-helpers/template-parts/form-horizontal.php
@@ -13,10 +13,10 @@ $form_subtitle = $form_arr['subtitle'];
         <form data-name="Заявка Москва" class="mt-n2" onsubmit="return false;">
           <div class="d-flex flex-wrap flex-lg-nowrap justify-content-between">
           <div class="w-75 mx-auto mx-lg-1 my-2 my-lg-0">
-              <input class="uk-input w-input" type="text" placeholder="Ваше имя" name="name" data-name="name" id="name">
+              <input class="uk-input w-input" type="text" placeholder="Ваше имя" name="name" data-name="name">
             </div>
             <div class="w-75 mx-auto mx-lg-1 my-2 my-lg-0">
-              <input class="uk-input w-input phone-input" type="text" placeholder="Номер телефона" name="phone" data-name="phone" id="phone">
+              <input class="uk-input w-input phone-input" type="text" placeholder="Номер телефона" name="phone" data-name="phone">
             </div>
             <div class="w-75 mx-auto mx-lg-1 my-2 my-lg-0">
               <input class="uk-input w-input" type="text" placeholder="Марка / Модель" name="car" data-name="car" id="car" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
--- theme-helpers/template-parts/modal-form.php
+++ theme-helpers/template-parts/modal-form.php
@@ -1,30 +1,39 @@
 <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModal" aria-hidden="true">
     <div class="modal-dialog modal modal-dialog-centered mt-n5">
-        <div class="modal-content px-3 py-4">
+        <div class="modal-content px-4 py-5">
             <div class="modal-body">
                 <div class="mb-2 d-flex">
                     <div class="ml-auto pl-5">
-                        <p class="h3">Заявка на займ</p>
-                        <p class="subtitle ml-auto mt-2 mb-3">Мы свяжемся с вами через минуту</p>
+                        <p class="h2 mt-3">Заявка на займ</p>
+                        <p class="subtitle ml-auto mt-2 mb-3">Мы свяжемся с вами <strong>через минуту</strong></p>
                     </div>
-                    <button type="button" class="ml-auto close align-self-start font-weight-light h1 mt-n3" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+                    <button type="button" class="ml-auto close align-self-start font-weight-light mt-n4" data-dismiss="modal" aria-label="Close">
+                        <svg version="1.1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve">
+                            <!-- Icons made by Freepik (www.flaticon.com/authors/freepik) from Flaticon (www.flaticon.com) -->
+                            <g>
+                                <g>
+                                    <path d="M284.286,256.002L506.143,34.144c7.811-7.811,7.811-20.475,0-28.285c-7.811-7.81-20.475-7.811-28.285,0L256,227.717
+                                        L34.143,5.859c-7.811-7.811-20.475-7.811-28.285,0c-7.81,7.811-7.811,20.475,0,28.285l221.857,221.857L5.858,477.859
+                                        c-7.811,7.811-7.811,20.475,0,28.285c3.905,3.905,9.024,5.857,14.143,5.857c5.119,0,10.237-1.952,14.143-5.857L256,284.287
+                                        l221.857,221.857c3.905,3.905,9.024,5.857,14.143,5.857s10.237-1.952,14.143-5.857c7.811-7.811,7.811-20.475,0-28.285
+                                        L284.286,256.002z"></path>
+                                </g>
+                            </g>
+                        </svg>
                     </button>
                 </div>
-                <form data-name="Заявка Москва" class="row" onsubmit="return false;">
-                    <div class="col-12 mb-4">
-                        <p class="text-left">Ваше имя*</p>
-                        <input class="uk-input w-input" type="text" name="name" data-name="name" id="name">
+                <form data-name="Заявка Москва" class="row mx-auto" onsubmit="return false;">
+                    <div class="w-100 mb-4">
+                        <p class="text-left">Ваше имя</p>
+                        <input class="uk-input w-input" type="text" name="name" data-name="name">
                     </div>
-                    <div class="col-12 mb-4">
+                    <div class="w-100 mb-4">
                         <p class="text-left">Ваш номер телефона*</p>
-                        <input class="uk-input w-input phone-input" type="text" placeholder="+7(___) ___-____" name="phone" data-name="phone" id="phone">
+                        <input class="uk-input w-input phone-input" type="text" placeholder="+7(___) ___-____" name="phone" data-name="phone">
                     </div>
-                    <div class="col-12 text-center mb-4">
-                        <button type="submit" data-wait="Пожалуйста, подождите." class="new-button">Оформить займ онлайн</button>
-                    </div>
-                    <div class="col-12 text-left mb-3">
-                        <label class="form-chek-small control control-checkbox font-basic">
+                    <button type="submit" data-wait="Пожалуйста, подождите." class="new-button w-100 mb-4">Оформить займ онлайн</button>
+                    <div class="w-100 mb-3 ml-n3">
+                        <label class="form-chek-small control control-checkbox font-basic nowrap">
                             <input class="uk-checkbox form-chek-small" type="checkbox" id="checkbox-10" name="checkbox-10" data-name="Checkbox 10" required="" checked=""> 
                             <noindex class=" font-weight-normal">Согласие с <a href="/privacy-policy.pdf" target="_blank" class=" link">политикой конфиденциальности</a>. </noindex> 
                             <div class="control_indicator"></div>
